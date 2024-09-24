c57610efd37c5ca2f4edbcf2eb119f838649a760
brock <brockfielder@cmail.carleton.ca>
2024-09-24 12:07:17 -0400

    dddddddd

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -11,13 +11,17 @@ public class AddressBook {
         collection.add(buddy);
     }
 
+    public ArrayList<BuddyInfo> getCollection() {
+        return collection;
+    }
+
     public void removeBuddy(BuddyInfo buddy) {
         collection.remove(buddy);
     }
 
 
 public static void main(String[] args) {
-    System.out.println("Address Book!!!");
+    System.out.println("Address Book!!!!!!!!!");
     AddressBook Book1= new AddressBook();
 
 
